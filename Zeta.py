b266a78e45acedfd073d48ef939f5c2697eb562f
theo <bequet_t@etna-alternance.net>
2021-05-06 15:18:03 +0200

    Graphe de zeta en fonction des angles d'incidence, avec une courbe par angle de reception, superposé. Probleme a partir de i = 160 car pas de fichier de ref

--- Zeta.py
+++ Zeta.py
@@ -1,25 +1,28 @@
 import numpy
 import scipy.integrate
+import os.path
 
 
 def Zcalc(i, r):  # i angle d'incidence, r angle de reception
-    if i < -60 or i > 60 or r < 20 or r > 340:
+    if r < -60 or r > 60 or i < 20 or i > 340:
         print("Wrong angle values")
         return 0
 
-    leafFile = "/Users/theo/Documents/INRA/Données/Mesures/2908/2908/feuille/type_txt/leaf"
-    refFile = "/Users/theo/Documents/INRA/Données/Mesures/2908/2908/reference/type_txt/Ref"
+    leafFile = "./data/2908/feuille/type_txt/leaf"
+    refFile = "./data/2908/reference/type_txt/Ref"
     i2 = int((i / 10) - 1)
-    r2 = int((r + 60) / 20)
+    r2 = int(((r + 60) / 20) + 1)
     if i2 >= 10:
-        leafFile = "a" + str(i2)
-        refFile = "a" + str(i2)
-    leafFile += str(i2)
-    refFile += str(i2)
-    leafData = numpy.loadtxt(leafFile + str(r2) + ".txt")
-    refData = numpy.loadtxt(refFile + str(r2) + ".txt")
-    lenData = numpy.loadtxt("/Users/theo/Documents/INRA/Données/Mesures/2908/2908/feuille/type_txt/Lambda.txt")
+        i2 = "a" + str(i2)
+    leafFile += str(i2) + str(r2) + ".txt"
+    refFile += str(i2) + str(r2) + ".txt"
 
+    if os.path.isfile(leafFile) and os.path.isfile(refFile):
+        leafData = numpy.loadtxt(leafFile)
+        refData = numpy.loadtxt(refFile)
+        lenData = numpy.loadtxt("./data/2908/feuille/type_txt/Lambda.txt")
+    else:
+        return 0
     rcData = []
     rcLenData = []
     rsData = []
@@ -33,16 +36,15 @@ def Zcalc(i, r):  # i angle d'incidence, r angle de reception
             rs = leafData[x] / refData[x]
             rsData.append(rs)
             rsLenData.append(lenData[x])
-    Rcalc(rcLenData, rcData)
-    RMeanCalc(rcData)
+    zeta = Rcalc(rcLenData, rcData) / Rcalc(rsLenData, rsData)
+    return zeta
 
 
 def Rcalc(lenData, rData):
     iR = scipy.integrate.simps(rData, lenData)
-    print(rData)
-    print(lenData)
-    print(iR / (lenData[-1] - lenData[0]))
+    return iR / (lenData[-1] - lenData[0])
+
 
 def RMeanCalc(rData):
-    mR = sum(rData) / len(rData)
-    print(mR)
+    mR = numpy.mean(rData)
+    return mR
